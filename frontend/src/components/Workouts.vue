f174b72b9544db2e69d8be1daebedf02856e9acb
kbade <bade.katharina@gmail.com>
2023-11-11 10:05:22 +0100

    frontend scoreboard and workout overview

---
+++ frontend/src/components/Workouts.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps(["name", "description", "pos"])
+</script>
+
+<template>
+  <div class="w-1/2 relative" :class="pos == -1 ? 'ml-6 mr-3' : (pos == 1 ? 'mr-6 ml-3' : 'mx-3')">
+    <img class="rounded-xl" src="../assets/pictures/yoga-vertical.jpg" alt="woman yoga">
+    <div class="bg-black h-full w-full absolute z-10 top-0 rounded-xl opacity-50"></div>
+    <div class="absolute bottom-0 mb-4 mx-4 z-20 text-white">
+      <p class="text-xl font-bold">{{ name }}</p>
+      <p>{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,20 +1,20 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 
 /* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
+import {library} from '@fortawesome/fontawesome-svg-core'
 
 /* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faHome, faMedal, faCog, faUser, faBell, faWalking } from '@fortawesome/free-solid-svg-icons'
-import {  } from '@fortawesome/free-regular-svg-icons'
-import {  } from '@fortawesome/free-brands-svg-icons'
+import {faHome, faMedal, faCog, faUser, faBell, faWalking, faTrophy} from '@fortawesome/free-solid-svg-icons'
+import {} from '@fortawesome/free-regular-svg-icons'
+import {} from '@fortawesome/free-brands-svg-icons'
 
 /* add icons to the library */
-library.add(faHome, faMedal, faCog, faUser, faBell, faWalking)
+library.add(faHome, faMedal, faCog, faUser, faBell, faWalking, faTrophy)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
